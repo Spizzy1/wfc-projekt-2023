47e4ce0e2e1876e93840b6d0518e7a3eb65c90de
Temp <Temp@5CD132CXH5.learnet.se>
2023-07-22 08:53:39 +0200

    commit

--- Test/main.cpp
+++ Test/main.cpp
@@ -5,10 +5,25 @@
 #include <SDL.h>
 #include <functional>
 #include <cmath>
+#include <vector>
+#include <tuple>
 
-struct chunk {
+#define resolution 100
+
+
+struct Chunk {
     float x, y;
 };
+
+int** surroundingtiles(Uint8** grid, int x, int y) {
+    std::tuple<int, int> coords[8];
+    for (int y = -1; y <= 1; y++) {
+        for (int x = -1; x <= -1; x++) {
+            
+            return false;
+        }
+    }
+}
 
 int main(int argv, char* argc[])
 {
@@ -21,15 +36,17 @@ int main(int argv, char* argc[])
     SDL_Window* window = SDL_CreateWindow("Wave function collapse implementation Mattekollo 2023",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, ww, wh, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
 
+    // create texture
+    const unsigned int texWidth = ww;
+    const unsigned int texHeight = wh;
+    SDL_Texture* winTex = SDL_CreateTexture( renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, texWidth, texHeight );
+    std::vector<uint32_t> pixels(texWidth * texHeight * 4, 0);
 
     float dt;
     // wfc stuff
-    const int resolution = 100;
     Uint8 grid[resolution][resolution];
     
     
-    //SDL_Surface* screen = SDL_CreateRGBSurface(0, ww, wh, 8, 0, 0, 0, SDL_ALPHA_OPAQUE);
-
     
     bool running = true;
     SDL_Event ev;
@@ -43,9 +60,26 @@ int main(int argv, char* argc[])
                 running = false;
             }
         }
+        
+        // splat down some random pixels
+        for( unsigned int i = 0; i < ww*wh; i++ )
+        {
+            const unsigned int x = rand() % texWidth;
+            const unsigned int y = rand() % texHeight;
+
+            const unsigned int offset = ( texWidth * y * 4 ) + x * 4;
+            pixels[ offset + 0 ] = rand() % 256;        // b
+            pixels[ offset + 1 ] = rand() % 256;        // g
+            pixels[ offset + 2 ] = rand() % 256;        // r
+            pixels[ offset + 3 ] = SDL_ALPHA_OPAQUE;    // a
+        }
+
+        SDL_UpdateTexture(winTex, nullptr, pixels.data(), texWidth * 4);
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
+
+		SDL_RenderCopy(renderer, winTex, nullptr, nullptr);
 
         SDL_RenderPresent(renderer);
 
